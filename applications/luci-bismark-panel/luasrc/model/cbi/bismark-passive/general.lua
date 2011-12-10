0f522d11970635b1164aa541ad14913fc49b2649
Sam Burnett <srburnet@gmail.com>
2011-12-10 21:01:28 +0000

    Fix name clash bug (I guess?)

--- applications/luci-bismark-panel/luasrc/model/cbi/bismark-passive/general.lua
+++ applications/luci-bismark-panel/luasrc/model/cbi/bismark-passive/general.lua
@@ -3,15 +3,15 @@ m.on_after_commit = function()
     luci.sys.call("/tmp/etc/init.d/bismark-passive restart")
 end
 
-s = m:section(TypedSection, "general", "")
+s = m:section(TypedSection, "globaloptions", "")
 s.anonymous = true
-s:tab("general", "General settings")
-s:taboption("general", Flag, "enabled", "Enable Bismark Passive", "If enabled, we will log limited information about traffic from your home network, such as when your devices access the Internet and when they contact sites on the whitelist (if enabled; see below).")
+s:tab("globaloptions", "General settings")
+s:taboption("globaloptions", Flag, "enabled", "Enable Bismark Passive", "If enabled, we will log limited information about traffic from your home network, such as when your devices access the Internet and when they contact sites on the whitelist (if enabled; see below).")
 
 s = m:section(TypedSection, "whitelist", "")
 s.anonymous = true
 s:tab("whitelist", "Domain whitelist")
-en = s:taboption("whitelist", Flag, "enabled", "Enable logging of whitelisted domains", "If enabled, we will log when devices connected to the BISmark router contact the sites on the list. <b>This does not record page contents, URLs, e-mail messages, or any other personal information.</b> For example, if you browse Facebook the software will only record the fact that you visited Facebook; it will record nothing about your username, your friends, your photos, etc.")
+en = s:taboption("whitelist", Flag, "enabled", "Enable logging of whitelisted domains", "If enabled, we will log when devices connected to the BISmark router contact the sites on the list, including subdomains. <b>This does not record page contents, URLs, e-mail messages, or any other personal information.</b> For example, if you browse Facebook the software will only record the fact that you visited Facebook; it will record nothing about your username, your friends, your photos, etc.")
 en.rmempty = false
 domain = s:taboption("whitelist", DynamicList, "domain", "Whitelisted domain names")
 domain.datatype = "host"
