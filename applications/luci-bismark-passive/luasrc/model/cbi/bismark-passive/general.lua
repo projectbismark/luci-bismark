1009e7effcde40e61591f116fcf4f9bfd0ad0278
Sam Burnett <srburnet@gmail.com>
2012-01-05 02:34:50 +0000

    Remove bismark-passive's global toggle

--- applications/luci-bismark-passive/luasrc/model/cbi/bismark-passive/general.lua
+++ applications/luci-bismark-passive/luasrc/model/cbi/bismark-passive/general.lua
@@ -3,11 +3,6 @@ m.on_after_commit = function()
     luci.sys.call("/tmp/etc/init.d/bismark-passive restart")
 end
 
-s = m:section(TypedSection, "globaloptions", "")
-s.anonymous = true
-s:tab("globaloptions", "General settings")
-s:taboption("globaloptions", Flag, "enabled", "Enable Bismark Passive", "If enabled, we will log limited information about traffic from your home network, such as when your devices access the Internet and when they contact sites on the whitelist (if enabled; see below). The <a href=\"https://github.com/sburnett/bismark-passive/blob/master/PRIVACY.md\">privacy statement</a> explains what data we collect and how we will use it.")
-
 s = m:section(TypedSection, "whitelist", "")
 s.anonymous = true
 s:tab("whitelist", "Domain whitelist")
